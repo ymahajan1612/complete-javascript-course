eb0a87814447c73edc29d2d727788ca425462444
Yash Mahajan <yash.mahajan@student.manchester.ac.uk>
2023-11-04 05:30:39 +0000

    starting strings in section 9

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,9 +1,52 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  return age;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     const output = `you are ${age} born in ${birthYear}`;
+//     console.log(output);
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+// hoisting with variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'yash';
+let job = 'teacher';
+const year = 1991;
+
+// function hoisting
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) {
+  deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
